488032d1f74b4d43616c63a72056c18984895d35
kaua212106 <hkaua7539@gmail.com>
2026-08-23 13:47:13 -0300

    Update cache version in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='meu-caderno-v4-central-guard';
+const CACHE='meu-caderno-v5-central-guard';
 const CORE=['./','./index.html','./manifest.json','./icone.png','./firebase-config.js','./firebase-sync.js','./auth-guard-v3.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
